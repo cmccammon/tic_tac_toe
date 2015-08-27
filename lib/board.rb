1a20bcc1bc6ae83b043fc2c7d404558698e3d181
Chris McCammon <cmccammon@gmail.com>
2015-08-27 14:54:35 -0500

    initial comm

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+class Board
+
+puts "Hi there",
+"would you like to play a game of Tic Tac Toe?"
+
+
+puts " 1 | 2 | 3 ",
+     "-----------",
+     " 4 | 5 | 6 ",
+     "-----------",
+     " 7 | 8 | 9 "
+print "\n"
+
+print "(Y)es or (N)o >"
+
+end
