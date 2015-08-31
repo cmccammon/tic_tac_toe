d22e11db5f67f5f129dad344c83175fda85bc48d
Chris McCammon <cmccammon@gmail.com>
2015-08-31 08:46:14 -0500

    troubl building classes

--- lib/game.rb
+++ lib/game.rb
@@ -5,26 +5,65 @@ class Player
 
   attr_accessor :player, :moves
 
-    def initialize
-      @player = player
+    def initialize(player1, player2)
+      @player = player #this needs to switch between player 1 and 2
       @moves
-
     end
-  end
+
+end
 
 class Game
-  attr_accessor :board, :taken_moves
+  attr_accessor :board, :taken
 
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @taken_moves = []
+    @taken = []
   end
+end
 
+def game_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} ",
+       "-----------",
+       " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
+       "-----------",
+       " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
 
+def winner?(player, win_table)#compairs board array to win table
+  win_table.each do |array|
+    if player.moves.include?(array[0]) && player.moves.include?(array[1]) && player.moves.include?(array[2])
+      puts "Game over: #{player} wins!"
+    end
+end
+
+def game_play(player1, player2)
+  game = Game.new
+  win_table = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  player1.moves = []
+  player2.moves = []
+
+  playing = true #this is the default
+  game.game_board
+
+  while playing #this is the game loop
+    if playing
+      game.check(player1.prompt("X", game.taken), "X")
+      game.game_board
+
+      if winner?(player1, win_table) || draw?(game)#player1 win check
+        playing = false
+      end
+    end
+
+    if playing
+      game.check(player2.prompt("O", game.taken), "O")
+      game.game_board
+
+      if winner?(player2, win_table) || draw?(game)#player2 win check
+        playing = false
+      end
+    end
   end
-
-win_table = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-
 
 
 def begin # the start of the game
@@ -32,72 +71,36 @@ def begin # the start of the game
   "would you like to play a game of Tic-Tac-Toe?"
   print "(Y)es or (N)o >"
   response = gets.chomp.upcase
-  player1 = Player.new
-  player2 = Player.new
   if response == 'Y'
     puts "You are now playing Tic-Tac-Toe!"
 
 end
-  loop do
 
-
-
+def choose
   puts "Player 1 is X choose a square"
-
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} ",
-       "-----------",
-       " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
-       "-----------",
-       " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-
   player1 = gets.chomp.to_i
-
-  # while player1.to_i < 10 && player1.to_i > 0 && player1.kind_of?(Integer) do
-  # player1 = gets.chomp.to_i
-  # else puts "invalid move try again"
-
-  # end
-
-
   @board[player1] = "X";
 
 
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} ",
-       "-----------",
-       " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
-       "-----------",
-       " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-
   puts "Player 2 is O choose a square"
   player2 = gets.chomp.to_i
-
   @board[player2] = "O";
 
+  @moves.push(next_move)
+end
+
+def draw
     if @board.all? {|i| i.is_a?(String) } == true
         puts "This game is a draw"
         break
     end
 end
 
-
-
   else
-
   puts "Maybe next time"
   end
 end
 
-# X is printed in array location of the board
-# array is checked for a winner
-#
-
-
-
-# >> Hi there
-# >> would you like to play a game of Tic-Tac-Toe?
-# >> (Y)es or (N)o >
-
-# ~> NoMethodError
-# ~> undefined method `chomp' for nil:NilClass
-# ~>
-# ~> /Users/chris/theironyard/week1/day4/tic_tac_toe/lib/game.rb:22:in `<main>'
+while begin?
+    game_play(player1, player2)
+  end
